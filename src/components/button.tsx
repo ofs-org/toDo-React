62b9afead888df0187091b1fc14556d0790a83fe
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-12-25 23:12:01 -0300

    refactor: Improved button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,51 +1,63 @@
 import Text from '@/components/text';
 import { cva, type VariantProps } from 'class-variance-authority';
 import type React from 'react';
+import Icon from './icon';
 
-const buttonVariants = cva('rounded-lg cursor-pointer px-4 py-4', {
-  variants: {
-    variant: {
-      primary: 'bg-blue',
+const buttonVariants = cva(
+  ' flex items-center transition-all rounded-lg cursor-pointer p-4',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-blue flex  hover:bg-blue-dark',
+      },
+      size: {
+        md: 'w-22.5 h-13 gap-2',
+      },
+      disabled: {
+        true: 'opacity-50  pointer-events-none',
+      },
     },
-    size: {
-      md: 'w-22.5 h-13',
-    },
-    desabilitado: {
-      true: 'opacity-50  cursor-not-allowed! pointer-none',
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
+      disabled: false,
     },
   },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'md',
-    desabilitado: false,
-  },
-});
+);
 
 interface ButtonProps
-  extends React.ComponentProps<'button'>,
+  extends Omit<React.ComponentProps<'button'>, 'size' | 'disabled'>,
     VariantProps<typeof buttonVariants> {
   children: React.ReactNode;
+  icon?: React.ComponentProps<typeof Icon>['svg'];
 }
 
 const Button = ({
   variant,
   size,
-  desabilitado,
+  disabled,
   children,
   className,
+  icon: IconComponent,
   ...props
 }: ButtonProps) => {
   return (
     <button
-      className={buttonVariants({ variant, desabilitado, size, className })}
+      className={buttonVariants({
+        variant,
+        disabled,
+        size,
+        className,
+      })}
       {...props}
     >
       <Text
         variant="sm"
-        className="flex items-center font-bold text-base-100! justify-center gap-2"
+        className="flex items-center font-bold text-base-100! justify-center"
       >
         {children}
       </Text>
+      {IconComponent && <Icon className="fill-base-100" svg={IconComponent} />}
     </button>
   );
 };
